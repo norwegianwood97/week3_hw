485474a0aa95b94f36c179e2a8bb935db7c26ef7
rosa1435 <158241447+rosa1435@users.noreply.github.com>
2024-03-01 12:06:58 +0900

    스키마 변경

--- src/routes/categories.router.js
+++ src/routes/categories.router.js
@@ -5,22 +5,25 @@ const router = express.Router();
 
 // 카테고리 등록 API
 router.post('/categories', async (req, res) => {
+  try {
+    const { name } = req.body; // req.body에서 name만 추출
 
-  const {name} = req.body
+    if (!name) {// 데이터 형식이 올바르지 않은 경우 클라이언트에게 에러 메시지 반환
+      return res.status(400).json({ errorMessage: "데이터 형식이 올바르지 않습니다." });
+    }
 
-
-  if (!name) {
-    return res.status(200).json({errerMessage: "데이터 형식이 올바르지 않습니다." })
-  }
-
-  const newCategory = await prisma.categories.create({ // 새 카테고리 생성
-    data: {
-        id: id,
+    await prisma.categories.create({
+      data: {
         name: name,
-        order: order,
-    },
-});
-return res.status(200).json({ message: "카테고리를 등록하였습니다." });
+      },
+    });
+
+    // 카테고리 생성 성공 응답
+    return res.status(201).json({ message: "카테고리를 등록하였습니다." });
+  } catch (error) {
+    // 서버 내부 오류 처리
+    return res.status(500).json({ errorMessage: "서버 에러가 발생했습니다.", errorDetails: error.message });
+  }
 });
 
 
